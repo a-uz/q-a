25453261dfbd45c978b2046f46005b2699e9d76a
Āris Uzars <aris.uzars@accenture.com>
2019-08-21 10:38:48 +0300

    Add layout styles

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,22 @@ import Question from './Question';
 import NewQuestion from './NewQuestion';
 import NotFound from './NotFound';
 
+import '../styles/App.css';
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path="/" component={Questions} />
-          <Route exact path="/questions/new" component={NewQuestion} />
-          <Route exact path="/questions/:id" component={Question} />
-          <Route component={NotFound} />
-        </Switch>
+
+        <main>
+          <Switch>
+            <Route exact path="/" component={Questions} />
+            <Route exact path="/questions/new" component={NewQuestion} />
+            <Route exact path="/questions/:id" component={Question} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
       </div>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Button from './Button';
+
+import '../styles/Header.css';
+
 function Header() {
   return (
-    <header>
-      <h1>Q&A</h1>
-      <Link to="/questions/new">Ask question</Link>
+    <header className="mainHeader">
+      <div>
+        <Link to="/">
+          <h1>Q&A</h1>
+        </Link>
+        <Link to="/questions/new">
+          <Button>Ask question</Button>
+        </Link>
+      </div>
     </header>
   );
 }
